33ac00e69cca404b6ffea2cfd333c49f74b83b06
Tree <gmldbs1109@naver.com>
2021-08-20 08:51:52 -0400

    BE-15 [feat] :  :tada: Swagger 구현 및 환경설정

--- server/src/main/java/com/teamspace/controller/UserController.java
+++ server/src/main/java/com/teamspace/controller/UserController.java
@@ -1,16 +1,13 @@
 package com.teamspace.controller;
 
 import com.teamspace.dto.LoginDTO;
-import com.teamspace.dto.UserInfoDTO;
 import com.teamspace.dto.UserResponseDTO;
-import com.teamspace.oauth.kakao.KakaoOauthService;
 import com.teamspace.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +15,13 @@ public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "카카오 로그인" , notes = "카카오 OAuth를 통해 로그인한다.")
     @PostMapping("/api/login/kakao")
     public ResponseEntity<UserResponseDTO> login(@RequestBody LoginDTO loginDTO) {
         return ResponseEntity.ok(userService.login(loginDTO));
     }
 
+    @ApiOperation(value = "카카오 로그아웃" , notes = "카카오 OAuth에서 로그아웃한다.")
     @GetMapping("/api/logout/kakao")
     public ResponseEntity logout(@RequestAttribute Long userId) {
         userService.logout(userId);
